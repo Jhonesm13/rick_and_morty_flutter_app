a56f9565e15ed6978a76b4f7c330b15f8d130e17
Jhonesm13 <jhone.menezes@edu.pucrs.br>
2025-08-29 21:45:02 -0300

    otimizado main

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,11 @@ import 'package:rick_and_morty_flutter/pages/details_page.dart';
 import 'package:rick_and_morty_flutter/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(MyHomePage());
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
